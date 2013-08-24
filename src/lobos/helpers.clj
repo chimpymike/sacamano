581d6381fdd40daab6ec490e11d58df8da408090
Michael Callahan <chimpymike@gmail.com>
2013-08-24 12:50:17 -0400

    Add lobos helper function to automatically add id column.

--- src/lobos/helpers.clj
+++ src/lobos/helpers.clj
@@ -1,3 +1,6 @@
 (ns lobos.helpers
   (:refer-clojure :exclude [bigint boolean char double float time])
   (:use [lobos schema]))
+
+(defn surrogate-key [table]
+  (integer table :id :auto-inc :primary-key))
