ca549f149e00dca799697ebf3aacef8fe490e076
Michael Callahan <chimpymike@gmail.com>
2013-08-24 12:47:17 -0400

    Add h2 dependency for dev database.

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
-                 [lobos "1.0.0-beta1"]]
+                 [lobos "1.0.0-beta1"]
+                 [com.h2database/h2 "1.3.173"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler sacamano.handler/app}
   :profiles
