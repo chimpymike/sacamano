8d591d151aa67f6bf164445ef8acbfbaf430c78d
Michael Callahan <chimpymike@gmail.com>
2013-08-27 16:40:03 -0400

    Add specific java.jdbc dependency for korma -- overrides lobos java.jdbc dependency.

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
                  [compojure "1.1.5"]
                  [lobos "1.0.0-beta1"]
                  [com.h2database/h2 "1.3.173"]
-                 [korma "0.3.0-RC5"]]
+                 [korma "0.3.0-RC5"]
+                 [org.clojure/java.jdbc "0.3.0-alpha4"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler sacamano.handler/app}
   :profiles
