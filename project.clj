b4e0e982c6ebf3cbcf23b28bf9472d8a7941d3b1
Michael Callahan <chimpymike@gmail.com>
2013-08-27 16:26:28 -0400

    Add korma dependency.

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
                  [lobos "1.0.0-beta1"]
-                 [com.h2database/h2 "1.3.173"]]
+                 [com.h2database/h2 "1.3.173"]
+                 [korma "0.3.0-RC5"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler sacamano.handler/app}
   :profiles
