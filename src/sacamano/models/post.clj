e454c944525644f26f43ce52f406df20882ed75f
Michael Callahan <chimpymike@gmail.com>
2013-08-28 11:45:57 -0400

    Require clojure.string for string functions.

--- src/sacamano/models/post.clj
+++ src/sacamano/models/post.clj
@@ -1,3 +1,4 @@
 (ns sacamano.models.post
   (:require [korma.core :refer :all])
-  (:require [sacamano.models.entities :refer :all]))
+  (:require [sacamano.models.entities :refer :all])
+  (:require [clojure.string :as str]))
