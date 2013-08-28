c928ddae125c82faf8daf1b8218d4577f3c01c19
Michael Callahan <chimpymike@gmail.com>
2013-08-28 11:58:23 -0400

    Add a function to make a post slug.

--- src/sacamano/models/post.clj
+++ src/sacamano/models/post.clj
@@ -2,3 +2,14 @@
   (:require [korma.core :refer :all])
   (:require [sacamano.models.entities :refer :all])
   (:require [clojure.string :as str]))
+
+(defn make-slug
+  "Make a slug from a string."
+  [s]
+  (let [s (str/lower-case s)
+        slug-regex #"[^a-z0-9]+"
+        trim-dashes-regex #"^-|-$"
+        replacement-char "-"]
+    (-> s
+        (str/replace slug-regex replacement-char)
+        (str/replace trim-dashes-regex ""))))
